efebc665564c48d45aa20ae5e81fc3e4423b870d
Juliana Meneguete <jumeneguete@gmail.com>
2021-04-01 19:30:06 -0300

    JS selecionando um de cada categoria

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function select (meal) {
+function selectMeal (meal) {
 
-    const dontDisplayBorder = document.querySelector(".selected");
-    const dontDisplayIcon = document.querySelector(".selected ion-icon");
+    const dontDisplayBorder = document.querySelector(".meal-options .selected");
+    const dontDisplayIcon = document.querySelector(".meal-options .selected ion-icon");
 
     if (dontDisplayBorder !== null){
         dontDisplayBorder.classList.remove("selected");
@@ -13,7 +13,38 @@ function select (meal) {
 
     let displayIcon = document.querySelector(".selected ion-icon");
     displayIcon.classList.remove("hide-icon");
-
-
 }
 
+function selectDrink (drink) {
+
+    const dontDisplayBorder = document.querySelector(".drink-options .selected");
+    const dontDisplayIcon = document.querySelector(".drink-options  .selected ion-icon");
+
+    if (dontDisplayBorder !== null){
+        dontDisplayBorder.classList.remove("selected");
+        dontDisplayIcon.classList.add("hide-icon");
+    }
+    
+    let displayBorder = document.querySelector(drink);
+    displayBorder.classList.add("selected");
+
+    let displayIcon = document.querySelector(".selected ion-icon");
+    displayIcon.classList.remove("hide-icon");
+}
+
+function selectDessert (dessert) {
+
+    const dontDisplayBorder = document.querySelector(".dessert-options .selected");
+    const dontDisplayIcon = document.querySelector(".dessert-options  .selected ion-icon");
+
+    if (dontDisplayBorder !== null){
+        dontDisplayBorder.classList.remove("selected");
+        dontDisplayIcon.classList.add("hide-icon");
+    }
+    
+    let displayBorder = document.querySelector(dessert);
+    displayBorder.classList.add("selected");
+
+    let displayIcon = document.querySelector(".selected ion-icon");
+    displayIcon.classList.remove("hide-icon");
+}
